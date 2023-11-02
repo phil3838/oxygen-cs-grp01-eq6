c43616e106b958bec70165a319d6085166c50254
phil3838 <cote3838@gmail.com>
2023-11-02 15:36:38 -0400

    pre-commit git hook fully repaired

--- src/main.py
+++ src/main.py
@@ -1,24 +1,33 @@
-from signalrcore.hub_connection_builder import HubConnectionBuilder
-import logging
-import requests
+"""
+This module serves to fetch the data and insert them in the database.
+"""
 import json
 import time
+import logging
+import requests
+
+from signalrcore.hub_connection_builder import HubConnectionBuilder
 
 
 class Main:
+    """
+    This is the main class responsible for managing connections and orchestrating
+    other operations.
+    """
+
     def __init__(self):
         """Setup environment variables and default values."""
         self._hub_connection = None
-        self.HOST = "https://hvac-simulator-a23-y2kpq.ondigitalocean.app"  # Setup your host here
-        self.TOKEN = "S1rN2enD5p"  # Setup your token here
+        self.host = "https://hvac-simulator-a23-y2kpq.ondigitalocean.app"  # Setup your host here
+        self.token = "S1rN2enD5p"  # Setup your token here
 
-        self.TICKETS = 2  # Setup your tickets here
-        self.T_MAX = 40  # Setup your max temperature here
-        self.T_MIN = 10  # Setup your min temperature here
-        self.DATABASE = None  # Setup your database here
+        self.tickets = 2  # Setup your tickets here
+        self.t_max = 40  # Setup your max temperature here
+        self.t_min = 10  # Setup your min temperature here
+        self.database = None  # Setup your database here
 
     def __del__(self):
-        if self._hub_connection != None:
+        if self._hub_connection is not None:
             self._hub_connection.stop()
 
     def setup(self):
@@ -38,7 +47,7 @@ class Main:
         """Configure hub connection and subscribe to sensor data events."""
         self._hub_connection = (
             HubConnectionBuilder()
-            .with_url(f"{self.HOST}/SensorHub?token={self.TOKEN}")
+            .with_url(f"{self.host}/SensorHub?token={self.token}")
             .configure_logging(logging.INFO)
             .with_automatic_reconnect(
                 {
@@ -77,14 +86,14 @@ class Main:
 
     def take_action(self, temperature):
         """Take action to HVAC depending on current temperature."""
-        if float(temperature) >= float(self.T_MAX):
+        if float(temperature) >= float(self.t_max):
             self.send_action_to_hvac("TurnOnAc")
-        elif float(temperature) <= float(self.T_MIN):
+        elif float(temperature) <= float(self.t_min):
             self.send_action_to_hvac("TurnOnHeater")
 
     def send_action_to_hvac(self, action):
         """Send action query to the HVAC service."""
-        r = requests.get(f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{self.TICKETS}")
+        r = requests.get(f"{self.host}/api/hvac/{self.token}/{action}/{self.tickets}")
         details = json.loads(r.text)
         print(details, flush=True)
 
--- test/test_main.py
+++ test/test_main.py
@@ -1,18 +1,25 @@
+"""
+This module serves to test main.py module.
+"""
 import unittest
 from unittest.mock import patch, MagicMock
 from src.main import Main
 
+
 class TestMain(unittest.TestCase):
+    """
+    This is the main test class.
+    """
 
     def setUp(self):
         self.main = Main()
 
-    @patch('src.main.HubConnectionBuilder')
+    @patch("src.main.HubConnectionBuilder")
     def test_set_sensorhub(self, MockHubConnectionBuilder):
         self.main.set_sensorhub()
         MockHubConnectionBuilder.assert_called_once()
 
-    @patch('src.main.requests.get')
+    @patch("src.main.requests.get")
     def test_send_action_to_hvac(self, mock_get):
         mock_response = MagicMock()
         mock_response.text = '{"status": "ok"}'
@@ -21,16 +28,19 @@ class TestMain(unittest.TestCase):
         action = "TurnOnAc"
         self.main.send_action_to_hvac(action)
 
-        expected_url = f"{self.main.HOST}/api/hvac/{self.main.TOKEN}/{action}/{self.main.TICKETS}"
+        expected_url = (
+            f"{self.main.host}/api/hvac/{self.main.token}/{action}/{self.main.tickets}"
+        )
         mock_get.assert_called_once_with(expected_url)
 
     def test_take_action(self):
-        with patch.object(self.main, 'send_action_to_hvac') as mock_send_action:
-            self.main.take_action(self.main.T_MAX + 1)
+        with patch.object(self.main, "send_action_to_hvac") as mock_send_action:
+            self.main.take_action(self.main.t_max + 1)
             mock_send_action.assert_called_with("TurnOnAc")
 
-            self.main.take_action(self.main.T_MIN - 1)
+            self.main.take_action(self.main.t_min - 1)
             mock_send_action.assert_called_with("TurnOnHeater")
+
 
 if __name__ == "__main__":
     unittest.main()
